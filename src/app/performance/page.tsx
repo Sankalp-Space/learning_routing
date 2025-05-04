9e83de8a07a918ced3199c49b2dd10ca56e85a3b
Sankalp-Space <sankalpshrivastav2001@gmail.com>
2025-05-04 23:16:51 +0530

    learning rouitng in Nextjs

---
+++ src/app/performance/page.tsx
@@ -0,0 +1,10 @@
+
+const PerformancePage= () =>{
+    return (
+        <div>
+            <h1>This is performance Page </h1>
+        </div>
+    )
+}
+
+export default PerformancePage;
---
+++ src/app/reliability/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReliabilityPage = () => {
+  return (
+    <div>
+      This is Reliability Page
+    </div>
+  )
+}
+
+export default ReliabilityPage;
