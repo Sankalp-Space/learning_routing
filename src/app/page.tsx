e0cbf7271c1c7972780b92b6c0a9726fc3616706
Sankalp-Space <sankalpshrivastav2001@gmail.com>
2025-05-05 00:14:04 +0530

    relative and absolute path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      My name is Sankalp Shrivastav
+      <nav className="container flex items-center justify-between mx-auto h-14">
+        <h1>Home</h1>
+        <div className="flex gap-2">
+          <h1>Performance</h1>
+          <h1>Reliability</h1>
+        </div>
+      </nav>
     </div>
   );
 }
