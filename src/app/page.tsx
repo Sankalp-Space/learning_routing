8f43289390c7c61736188424abfee9300d5feb6a
Sankalp-Space <sankalpshrivastav2001@gmail.com>
2025-05-07 22:27:05 +0530

    some more changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
+import NavbarPage from "@/components/header";
+import Imagination from "@/components/imagination";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <nav className="container flex items-center justify-between mx-auto h-14">
-        <h1>Home</h1>
-        <div className="flex gap-2">
-          <h1>Performance</h1>
-          <h1>Reliability</h1>
-        </div>
-      </nav>
+      <Imagination/>
+      <NavbarPage/>
+        
     </div>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import Link from 'next/link';
 
 const NavbarPage = () => {
   return (
     <div>
        <nav className="container flex items-center justify-between mx-auto h-14">
-        <h1>Home</h1>
+        <Link href="/">Not the Home Page</Link>
         <div className="flex gap-2">
-          <h1>Performance</h1>
-          <h1>Reliability</h1>
+          <Link href="/performance">Performance</Link>
+          <Link href="reliability">Reliability</Link>
         </div>
         </nav>
     </div>
